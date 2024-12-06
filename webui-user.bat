5ed5b00f3fd3c2edd1416718f2ce3a54bb1ac480
taofr5 <takozouma@gmail.com>
2024-12-06 18:55:38 +0900

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --theme dark --skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate --medvram  --disable-nan-check --opt-sub-quad-attention --no-half --precision full
 
 call webui.bat
